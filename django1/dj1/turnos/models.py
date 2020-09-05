3548bd361216ca307596230492c1b6f4ba282a7b
salva12 <salvamanu12@mail.com>
2020-09-05 19:32:01 -0300

    agregue la clase cliente a models

--- django1/dj1/turnos/models.py
+++ django1/dj1/turnos/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Cliente(models.Model):
+	dni = models.IntegerField()
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    direccion = models.CharField(max_length=30)
+    tel = models.CharField(max_length=30)
+	mail = models.EmailField(max_length=30)
+
