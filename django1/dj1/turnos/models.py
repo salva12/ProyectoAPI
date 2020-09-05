01f8f5913062ed9476d4b7f17c227deffa0ac444
Adrian 123 <santillanadrianalberto@hotmail.com>
2020-09-05 19:39:50 -0300

    agregue el modelo Proveedor

--- django1/dj1/turnos/models.py
+++ django1/dj1/turnos/models.py
@@ -10,3 +10,13 @@ class Cliente(models.Model):
     tel = models.CharField(max_length=30)
 	mail = models.EmailField(max_length=30)
 
+class Proveedor(models.Model):
+	cuit = models.IntegerField()
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    direccion = models.CharField(max_length=30)
+    tel = models.CharField(max_length=30)
+	mail = models.EmailField(max_length=30)
+    descrpcion= models.CharField(max_length=30)
+    rubro= models.CharField(max_length=30)
+
